048ad8fdf1931894a5ff4eb3c55f6afcb1fbe2fd
Jan Hybš <jan.hybs@tul.cz>
2015-10-20 18:27:26 +0200

    Fix path in shell

--- configs/test_flow.sh
+++ configs/test_flow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /storage/praha1/home/jan-hybs/projects/Flow123d/
-cd flow123d
+cd /storage/praha1/home/jan-hybs/projects/FlowRunner/
+cd src
 python flow/tests/script_run_tests.py --flow123d="/storage/praha1/home/jan-hybs/projects/Flow123d/flow123d/build_tree/bin/flow123d" --mpiexec="/storage/praha1/home/jan-hybs/projects/Flow123d/flow123d/build_tree/bin/mpiexec" --ndiff="/storage/praha1/home/jan-hybs/projects/Flow123d/flow123d/ndiff/ndiff.pl" --test-root="/storage/praha1/home/jan-hybs/projects/Flow123d/flow123d/tests" --tests-output="/storage/praha1/home/jan-hybs/projects/__output"
