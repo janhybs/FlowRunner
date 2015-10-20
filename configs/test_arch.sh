64cb9418e8e9b49c686036cdf03dd34eac0aa1b3
Jan Hybš <jan.hybs@tul.cz>
2015-10-20 18:21:03 +0200

    Update shell paths

--- configs/test_arch.sh
+++ configs/test_arch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /storage/praha1/home/jan-hybs/projects/FlowRunner/
 cd src
-python environment.py -o environment.json arch.cpu.avail=4 arch.memory.avail=4g
-python performance.py -o performance.json -c 1 -c 2 -c 3 -c 4
+python testing/script_environment.py -o /storage/praha1/home/jan-hybs/projects/__output/environment.json arch.cpu.avail=4 arch.memory.avail=4g
+python testing/script_performance.py -o /storage/praha1/home/jan-hybs/projects/__output/performance.json -c 1 -c 2 -c 3 -c 4
