5893eec5f04d3b7947f3e3882567ea2d39a6572c
Jan Hybš <jan.hybs@tul.cz>
2015-10-25 16:38:44 +0100

    Fix libs default path [ci skip]

--- src/pbs/script_run_pbs.py
+++ src/pbs/script_run_pbs.py
@@ -4,8 +4,9 @@
 
 import sys, os
 sys.path.append(os.getcwd())
-sys.path.append(os.path.join(os.getcwd(), 'libs'))
-sys.path.append(os.path.join(os.getcwd(), 'lib'))
+sys.path.append(os.path.join(os.getcwd(), '..', 'libs'))
+sys.path.append(os.path.join(os.getcwd(), '..', 'lib'))
+
 
 import json
 from pbs.pbsscript import PBSScript
