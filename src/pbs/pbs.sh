7e8880329840ba27ad84c2874648ff8f15314472
Jan Hybš <jan.hybs@tul.cz>
2015-10-26 17:10:13 +0100

    Edit test_flow script

--- src/pbs/pbs.sh
+++ src/pbs/pbs.sh
@@ -26,4 +26,9 @@ CPU_MEMORY_AVAIL="4gb"
 #!/bin/bash
 cd /storage/praha1/home/jan-hybs/projects/FlowRunner/
 cd src
-python flow/tests/script_run_tests.py --flow-root="/storage/praha1/home/jan-hybs/projects/Flow123d/flow123d"
+python testing/script_environment.py -o /storage/praha1/home/jan-hybs/projects/__output/environment.json arch.cpu.avail=4 arch.memory.avail=4g
+python testing/script_performance.py -o /storage/praha1/home/jan-hybs/projects/__output/performance.json -c 1 -c 2 -c 3 -c 4
+#!/bin/bash
+cd /storage/praha1/home/jan-hybs/projects/FlowRunner/
+cd src
+python flow/tests/script_run_tests.py --flow-root="/storage/praha1/home/jan-hybs/projects/Flow123d/flow123d" --output-timestamp-dir=""
