2edca7b5b1641760f487b0b722917559e04e360e
Jan Hybš <jan.hybs@tul.cz>
2015-10-26 16:39:25 +0100

    Simplify system.python module

--- src/system/python.py
+++ src/system/python.py
@@ -3,9 +3,7 @@
 # author:   Jan Hybs
 
 import os, sys
-from utils.logger import Logger
 
-logger = Logger(__name__)
 required_version = (2, 7, 0)
 
 
@@ -20,18 +18,18 @@ def init(debug=True):
     """
 
     if debug:
-        logger.debug('Running python {version}, {info}'.format(version=version(), info=sys.version_info))
+        print('Running python {version}, {info}'.format(version=version(), info=sys.version_info))
 
     if sys.version_info < required_version:
-        logger.error(
+        print(
             'Python version is too low {version} please use higher version, recommended version is {rec}'.format(
                 version=version(), rec=required_version))
         sys.exit(1)
 
     if debug:
-        logger.debug('Updating sys.path...')
+        print('Updating sys.path...')
     sys.path.append(os.path.join(os.getcwd(), '..', 'libs'))
     sys.path.append(os.path.join(os.getcwd(), '..', 'lib'))
 
     if debug:
-        logger.debug('Current sys.path:\n{paths}'.format(paths='\n'.join(sys.path)))
+        print('Current sys.path:\n{paths}'.format(paths='\n'.join(sys.path)))
