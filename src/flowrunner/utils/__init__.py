0520bf9858c82a298fc99044f7d0a0832059e86f
Jan Hybš <jan.hybs@tul.cz>
2015-10-30 21:57:37 +0100

    Add functions to recursive get and pluck obj

--- src/flowrunner/utils/__init__.py
+++ src/flowrunner/utils/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # author:   Jan Hybs
+from pluck import pluck
 
 
 def merge_dict(d=dict(), *args):
@@ -8,3 +9,23 @@ def merge_dict(d=dict(), *args):
     for arg in args:
         dict_copy.update(arg)
     return dict_copy
+
+
+def rget(obj, path, default=None):
+    names = path.split('.')
+    o = obj
+    for name in names:
+        if name in o:
+            o = o[name]
+        else:
+            return default
+    return o
+
+
+def rpluck(obj, path):
+    names = path.split('.')
+    last = names[-1]
+    del names[-1]
+
+    o = rget(obj, '.'.join(names), [])
+    return pluck(o, last)
