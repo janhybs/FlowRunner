33a72d750f0ed5ec14aa264cd1710adcd28e3f34
Jan Hybš <jan.hybs@tul.cz>
2015-10-30 15:11:43 +0100

    Update pbs script, lower no. CPUs

--- src/run-pbs.sh
+++ src/run-pbs.sh
@@ -2,7 +2,7 @@
 #PBS -N flow-test-production
 #PBS -l mem=6gb
 #PBS -l scratch=1gb
-#PBS -l nodes=1:ppn=5
+#PBS -l nodes=1:ppn=4
 #PBS -l walltime=00:30:00
 #PBS -j oe
 #PBS -m abe
@@ -19,7 +19,7 @@ module add python26-modules-gcc
 module add numpy-py2.6
 module add python-2.7.6-gcc
 
-CPU_ARCH_AVAIL="5"
+CPU_ARCH_AVAIL="4"
 CPU_MEMORY_AVAIL="6442450944"
 
 
@@ -32,4 +32,4 @@ python flowrunner/testing/script_performance.py -o /auto/praha1/jan-hybs/project
 
 cd /storage/praha1/home/jan-hybs/projects/FlowRunner/src
 # run benchmark tests on 1, 2, 3, 4, 6 and 8 CPUs
-python flowrunner/flow/tests/script_run_tests.py --flow-root="/storage/praha1/home/jan-hybs/projects/Flow123d/flow123d" --output-timestamp-dir="" --test-root="/auto/praha1/jan-hybs/projects/tests" --nproc=1:5 --nproc=6 --nproc=8
+python flowrunner/flow/tests/script_run_tests.py --nproc=1:5 --nproc=6 --nproc=8 --flow-root="/storage/praha1/home/jan-hybs/projects/Flow123d/flow123d"  --test-root="/auto/praha1/jan-hybs/projects/tests" --output-timestamp-dir=""
