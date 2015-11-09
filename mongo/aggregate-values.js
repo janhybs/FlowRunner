ec4bfda449e887eef278b07927b06eb7d9d86a46
Jan_Hybs <jan.hybs@tul.cz>
2015-11-09 16:08:34 +0100

    Add more commands

--- mongo/aggregate-values.js
+++ mongo/aggregate-values.js
@@ -4,3 +4,6 @@ var result = db.pts.aggregate([
 ]).result
 
 result
+
+db.environment.find({}, {_id: 0, calibration: 1, 'arch.system.node': 1})
+db.environment.aggregate([{$project: {_id: 0, 'cpu': '$calibration.cpu','memory': '$calibration.memory', 'node': '$arch.system.node'}}]).result
