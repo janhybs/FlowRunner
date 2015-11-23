79b3838a157f1f51eee59d1f6eb823fb77fe2a7e
Jan_Hybs <jan.hybs@tul.cz>
2015-11-23 18:59:57 +0100

    Edit accumulation

--- src/flowrunner/db/mongo_pts_aggregate.py
+++ src/flowrunner/db/mongo_pts_aggregate.py
@@ -14,53 +14,52 @@ import numpy as np
 from operator import itemgetter
 
 true = True
-mil = 1/1000000.0
+mil = 1 / 1000000.0
 mongo = MongoDB()
 cursor = mongo.collections.pts.aggregate(
     [
-    { '$match': {
-        'context.version': {'$exists': true},
-        # 'path': ",whole-program,hc-run-simulation,tos-one-step,convection-one-step,mat-mult,",
+        { '$match': {
+            'context.version': { '$exists': true },
+            # 'path': ",whole-program,hc-run-simulation,tos-one-step,convection-one-step,mat-mult,",
         }
-    },
-    # {
-    #   '$group': {
-    #       '_id': {
-    #           'path': '$path',
-    #           'nproc': '$context.nproc',
-    #           'problem': '$context.problem',
-    #           'version': {'$substr': ['$context.version', 18, 1]},
-    #       },
-    #       'duration': {'$push': {'$multiply': ['$metrics.duration', 1000]}}
-    #     }
-    # },
-    { '$project': {
-        'duration': {'$multiply': ['$metrics.duration', 1000000]},
-        '_id': {
-            'path': '$path',
-            'nproc': '$context.nproc',
-            'problem': '$context.problem',
-            'version': {'$substr': ['$context.version', 18, 1]},
+        },
+        # {
+        # '$group': {
+        #       '_id': {
+        #           'path': '$path',
+        #           'nproc': '$context.nproc',
+        #           'problem': '$context.problem',
+        #           'version': {'$substr': ['$context.version', 18, 1]},
+        #       },
+        #       'duration': {'$push': {'$multiply': ['$metrics.duration', 1000]}}
+        #     }
+        # },
+        { '$project': {
+            'duration': { '$multiply': ['$metrics.duration', 1000000] },
+            '_id': {
+                'path': '$path',
+                'nproc': '$context.nproc',
+                'problem': '$context.problem',
+                'version': { '$substr': ['$context.version', 18, 1] },
             }
         }
-    },
-        {'$sort':
-             SON([
-                 ('_id.path', 1),
-                 ('_id.nproc', 1),
-                 ('_id.problem', 1),
-                 ('_id.version', 1),
-             ])
+        },
+        { '$sort':
+              SON([
+                  ('_id.path', 1),
+                  ('_id.nproc', 1),
+                  ('_id.problem', 1),
+                  ('_id.version', 1),
+              ])
         }
     ])
-
 
 aggr_result = fetch_all(cursor)
 
 #
 # def foo(x):
 # if type(x) is not list:
-#         return str(x)
+# return str(x)
 #     return [str(i) for i in x]
 #
 #
@@ -85,7 +84,7 @@ def categorize(items, prop_func):
     :rtype : dict
     """
     getter = prop_func if callable(prop_func) else lambda x: rget(x, prop_func, None)
-    result = {}
+    result = { }
     for item in items:
         key = getter(item)
         if key in result:
@@ -93,6 +92,7 @@ def categorize(items, prop_func):
         else:
             result[key] = [item]
     return result
+
 
 def _get_short_name(k):
     v = k.strip(',').split(',')
@@ -103,13 +103,13 @@ def multi_cat(items, cats=[]):
     last_cat = cats[-1]
     del cats[-1]
 
-    result = {}
+    result = { }
     for item in items:
         current = result
         for cat in cats:
             key = cat(item)
             if key not in current:
-                current[key] = {}
+                current[key] = { }
             current = current[key]
 
         last_key = last_cat(item)
@@ -118,39 +118,43 @@ def multi_cat(items, cats=[]):
         current[last_key].append(item)
     return result
 
-foo = multi_cat(aggr_result, [
-    lambda x: rget(x, '_id.problem', None),
-    lambda x: rget(x, '_id.path', None),
-    lambda x: rget(x, '_id.nproc', None),
-    lambda x: rget(x, '_id.version', None)])
+
+# foo = multi_cat(aggr_result, [
+#     lambda x: rget(x, '_id.problem', None),
+#     lambda x: rget(x, '_id.path', None),
+#     lambda x: rget(x, '_id.nproc', None),
+#     lambda x: rget(x, '_id.version', None)])
 
 
-difference = list()
-for problem_name, problem_items in foo.items():
-    # print '- %s' % problem_name
-    for path_name, path_items in problem_items.items():
-        # print '-- %s' % path_name
-        for nproc_name, nproc_items in path_items.items():
-            # print '--- %s' % nproc_name
-
-            keys = sorted(nproc_items.keys())
-            keys = ['A', 'B']
-            for key_index in range(0, len(keys)-1):
-                cur = nproc_items[keys[key_index]]
-                nxt = nproc_items[keys[key_index+1]]
-
-                cur_dur = float(np.mean(pluck(cur, 'duration')))
-                nxt_dur = float(np.mean(pluck(nxt, 'duration')))
-                # diff = (nxt_dur/cur_dur*100.0) - 100.0
-                diff = (nxt_dur / cur_dur - 1.0) * 1.0
-                difference.append({
-                    'name': path_name,
-                    'nproc': str(nproc_name),
-                    'problem': problem_name,
-                    'time_frame': _get_short_name(path_name),
-                    'result': diff
-                })
-                # print '---- {:6.0f} {:6.0f} {:5.2f}'.format(cur_dur, nxt_dur, nxt_dur/cur_dur)
+#
+# exit()
+# difference = list()
+# for problem_name, problem_items in foo.items():
+#     # print '- %s' % problem_name
+#     for path_name, path_items in problem_items.items():
+#         # print '-- %s' % path_name
+#         for nproc_name, nproc_items in path_items.items():
+#             # print '--- %s' % nproc_name
+#
+#             keys = sorted(nproc_items.keys())
+#             keys = ['A', 'B']
+#             for key_index in range(0, len(keys)-1):
+#                 cur = nproc_items[keys[key_index]]
+#                 nxt = nproc_items[keys[key_index+1]]
+#
+#                 cur_dur = float(np.mean(pluck(cur, 'duration')))
+#                 nxt_dur = float(np.mean(pluck(nxt, 'duration')))
+#                 result = sum (pluck(nxt, 'duration')) / sum(pluck(cur, 'duration'))
+#                 # diff = (nxt_dur/cur_dur*100.0) - 100.0
+#                 diff = (nxt_dur / cur_dur - 0.0) * 1.0
+#                 difference.append({
+#                     'name': path_name,
+#                     'nproc': str(nproc_name),
+#                     'problem': problem_name,
+#                     'time_frame': _get_short_name(path_name),
+#                     'result': result
+#                 })
+#                 # print '---- {:6.0f} {:6.0f} {:5.2f}'.format(cur_dur, nxt_dur, nxt_dur/cur_dur)
 
 
 
@@ -162,12 +166,12 @@ def find_hotspot(samples, delta):
     hotspots = []
     for k, g in groups:
         l = list(g)
-        c =float(len(l))
+        c = float(len(l))
         sn = _get_short_name(k)
         hotspots.append({
             'name': k,
             'short_name': sn,
-            'result': sum([x['result'] for x in l]),
+            'result': sum([x['result'] for x in l]) / c,
             'count': c,
             'freq': c / len(diff_sample),
             sn: c
@@ -183,8 +187,8 @@ def find_hotspot(samples, delta):
     return hotspots[:]
 
 
-def group_by (items, group_prop_func):
-    result = {}
+def group_by(items, group_prop_func):
+    result = { }
     grp = []
     cur_grp = None
     for item in items:
@@ -202,21 +206,28 @@ def group_by (items, group_prop_func):
 
 
 def reduce_groups(items, reductions=[]):
-    result = {}
+    result = { }
     for key, grp in items.items():
-        item = {'_id': key, '_count': len(grp)}
+        item = { '_id': key, '_count': len(grp) }
         for reduce_field, new_field, reduce_func in reductions:
             item[new_field] = reduce_func(pluck(grp, reduce_field))
         result[key] = item
     return result
 
+
 def group_by_name(x):
     return x['name']
+
 
 def grouper_by_name_and_cpu(x):
     return x['name'] + x['nproc']
 
-def detect_hostspot (slowdown_factors, epsilon=0.1):
+
+def grouper_by_time_frame(x):
+    return _get_short_name(x['path'])
+
+
+def detect_hostspot(slowdown_factors, epsilon=0.1):
     partial_sample = slowdown_factors[0:int(len(slowdown_factors) * epsilon)]
     partial_sample = sorted(partial_sample, key=grouper)
 
@@ -224,8 +235,8 @@ def detect_hostspot (slowdown_factors, epsilon=0.1):
     reducted_factors = reduce_groups(grouped_factors, [
         ('time_frame', 'time_frame', lambda x: x[0]),
         ('result', 'result', sum),
-        ('result', 'weighted_result', lambda x: (int(sum(x) / len(x)*10000))/100.0),
-        ('nproc', 'nprocs', lambda _:_),
+        ('result', 'weighted_result', lambda x: (int(sum(x) / len(x) * 10000)) / 100.0),
+        ('nproc', 'nprocs', lambda _: _),
     ])
     hotspots = sorted(reducted_factors.values(), key=itemgetter('weighted_result'), reverse=True)
     # hotspots = hotspots[:10]
@@ -235,38 +246,74 @@ def detect_hostspot (slowdown_factors, epsilon=0.1):
     return hotspots
 
 
+versions = multi_cat(aggr_result, [
+    # lambda x: rget(x, '_id.problem', None),
+    lambda x: rget(x, '_id.version', None),
+    lambda x: _get_short_name(rget(x, '_id.path', None)),
+    # lambda x: rget(x, '_id.nproc', None),
+
+])
+
+
+# time_frames = group_by(versions, grouper_by_time_frame)
+for version_name, version_items in versions.items():
+    for time_frame_name, time_frame_items in version_items.items():
+        versions[version_name][time_frame_name] = sum(pluck(time_frame_items, 'duration'))
+
+version_A = 'A'
+version_B = 'B'
+
+versions['AB'] = { }
+for time_frame_name, time_frame_value in versions[version_A].items():
+    versions['AB'][time_frame_name] = versions[version_B][time_frame_name] / versions[version_A][time_frame_name]
+
+slow_down_list = []
+for time_frame_name, time_frame_value in versions['AB'].items():
+    slow_down_list.append({ 'name': time_frame_name, 'slow_down_factor': time_frame_value })
+
+n = 5
+slow_down_list = sorted(slow_down_list, key=itemgetter('slow_down_factor'), reverse=True)
+print '\\textbf{time-frame}&'+'&'.join(pluck(slow_down_list[:n], 'name')) +  '\\\\ \\hline'
+print '$SF_{ij}$&'+'&'.join(['%1.2f' % (x*100) for x in pluck(slow_down_list[:n], 'slow_down_factor')]) + '\\\\ \\hline'
+
+# slow_down = reduce_groups(time_frames, [
+#     ('duration', 'slow_down', sum)
+# ])
+# print slow_down
+#
 grouper = group_by_name
-difference = sorted(difference, key=itemgetter('result'), reverse=True)
-
-
-hotspots_measurements = []
-hotspots_list = []
-epsilons = [math.pow((x+1)/100.0, 2) for x in range(0, 106, 3)]
-for epsilon in epsilons:
-    print str(epsilon).replace('.', ',')
-    hotspots = detect_hostspot(difference, epsilon)
-    # print epsilon
-    # print pluck(hotspots[:8], 'time_frame')
-    # print pluck(hotspots[:8], 'weighted_result')
-    hotspots_measurements.append(hotspots)
-    hotspots_list.extend(hotspots)
-
-meas_groups = group_by(hotspots_list, itemgetter('time_frame'))
-meas_groups = reduce_groups(meas_groups, [('weighted_result', 'result', sum)])
-meas_groups = sorted(meas_groups.values(), key=itemgetter('result'), reverse=True)
-names = pluck(meas_groups, '_id')[:13]
-
-print '\t'.join(names).replace('convectiontransport', 'convection-transport').replace('transportoperatorspliting', 'transport-operator-spliting')
-for m in hotspots_measurements:
-    values = []
-    for name in names:
-        value = '0'
-        for e in m:
-            if e['time_frame'] == name:
-                value = str(e['result'])
-                break
-        values.append(value)
-    print '\t'.join(values).replace('.', ',')
+# difference = sorted(difference, key=itemgetter('result'), reverse=True)
+#
+#
+# hotspots_measurements = []
+# hotspots_list = []
+# epsilons = [math.pow((x+1)/100.0, 2) for x in range(0, 106, 3)]
+# for epsilon in epsilons:
+#     # print str(epsilon).replace('.', '.')
+#     hotspots = detect_hostspot(difference, epsilon)
+#     # print epsilon
+#     # print pluck(hotspots[:8], 'time_frame')
+#     # print pluck(hotspots[:8], 'weighted_result')
+#     hotspots_measurements.append(hotspots)
+#     hotspots_list.extend(hotspots)
+#
+# meas_groups = group_by(hotspots_list, itemgetter('time_frame'))
+# meas_groups = reduce_groups(meas_groups, [('weighted_result', 'result', sum)])
+# meas_groups = sorted(meas_groups.values(), key=itemgetter('result'), reverse=True)
+# names = pluck(meas_groups, '_id')[:13]
+#
+# print '\t'.join(names).replace('convectiontransport', 'convection-transport')\
+#     .replace('transportoperatorspliting', 'transport-operator-spliting')
+# for m in hotspots_measurements:
+#     values = []
+#     for name in names:
+#         value = '0'
+#         for e in m:
+#             if e['time_frame'] == name:
+#                 value = str(e['result'])
+#                 break
+#         values.append(value)
+#     print '\t'.join(values).replace('.', '.')
 
 
 # overall_performance_drop = detect_hostspot(difference, 1)[:6]
